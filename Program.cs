61b98779a87007af1e70dc1545c62ce7a1855bca
Beray Boztepe <44292203+berayboztepe@users.noreply.github.com>
2020-03-13 02:17:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Consoletry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //çıktı yazdırmak için kullanılır..
+            Console.WriteLine("Hello World!");
+            //bir tuşa basana kadar konsolu açık tutması için kullanılır..
+            // Console.ReadLine();
+
+            string a = "3";
+            string b = "5";
+            string c = "5";
+            Console.WriteLine(a + b + c);
+
+            // sayıları string olarak girmişken integera çevirdik...
+            int d = Convert.ToInt32(a) + Convert.ToInt32(b) + Convert.ToInt32(c);
+            Console.WriteLine(d);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
