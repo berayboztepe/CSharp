548466f0f909b8ee2694444b592a7dd0e22c7f19
Beray Boztepe <44292203+berayboztepe@users.noreply.github.com>
2020-03-17 15:51:47 +0300

    Add files via upload

---
+++ Bir string karakterin kelimelerini düz ve tersten yazdırma.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vaow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Girilen bir kelimeyi harf harf alt alta yazdıran program
+            Console.WriteLine("Bir kelime giriniz:");
+            string kelime = Console.ReadLine();
+
+            foreach (char karakter in kelime)
+            {
+                Console.WriteLine(karakter);
+            }
+            //Girilen bir metni tersten yazdıran program
+            Console.WriteLine("Bir kelime giriniz:");
+            string kelime1 = Console.ReadLine();
+
+            for (int i = kelime1.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(kelime1[i]);
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Listelerle ilgili birkaç kavram.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vaow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sayilar = new List<int>();
+            sayilar.Add(5);
+            sayilar.Add(3);
+            sayilar.Add(2);
+            sayilar.Add(9);
+            sayilar.Add(7);
+            sayilar.Add(15);
+
+            Console.WriteLine(sayilar[2]);
+            sayilar.Remove(2);
+            Console.WriteLine(sayilar[2]);
+
+
+            //belirtlien indexteki sayıyı atmak için
+            sayilar.RemoveAt(2);
+            Console.WriteLine(sayilar[2]);
+
+            //uzunluk bulmak için
+            int uzunluk = sayilar.Count;
+            Console.WriteLine("Uzunluk = " + uzunluk);
+
+            //listenin içinde belirtilen sayı var mı yok mu bulmak için
+            Console.WriteLine(sayilar.Contains(15));
+
+            //listenin tamamını temizlemek
+            sayilar.Clear();
+            Console.ReadLine();
+
+        }
+    }
+}
