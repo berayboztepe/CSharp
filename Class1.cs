a90f0361a33fd90ff2c64ac2693fa64c498c7fce
Beray Boztepe <44292203+berayboztepe@users.noreply.github.com>
2020-10-25 20:26:55 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DortIslem
+{
+    public class MatematikselIslemler
+    {
+        public static int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int Cikar(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+    }
+}
