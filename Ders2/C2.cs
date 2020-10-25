d2fbce13b9ac8566af6cd707b1cd5a3fc1a0d395
Beray Boztepe <44292203+berayboztepe@users.noreply.github.com>
2020-10-25 20:24:56 +0300

    Add files via upload

---
+++ Ders2/C2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ilk Sayiyi Girin: ");
+            var sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ikinci Sayiyi Girin: ");
+            var sayi2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iki Sayının Toplami = "+ DortIslem.MatematikselIslemler.Topla(sayi1, sayi2));
+            //Diğer classta Topla sınıfı static olduğu için burada, static main'de o sınıfı çalıştırmak için nesne oluşturmadan bu yöntemle
+            //çalıştırabiliriz!!!
+            DortIslem.MatematikselIslemler cikarma = new DortIslem.MatematikselIslemler();
+            //Ancak Cikar sınıfı static olmadığı için, çalıştırmak için nesne oluşturmamız gerekir!!!!
+            Console.WriteLine("Iki Sayının Farkı = "+cikarma.Cikar(sayi1, sayi2));
+
+            Console.ReadLine();
+        }
+    }
+}
