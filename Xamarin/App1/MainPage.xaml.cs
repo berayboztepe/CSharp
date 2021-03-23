9b5b938905d0d7d9794d75b9ee7da319754620da
Beray Boztepe <berayboztepe@outlook.com>
2021-03-23 17:02:43 +0100

    Add files via upload

---
+++ Xamarin/App1/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try {
+                float h = Convert.ToSingle(Height.Text);
+                float w = Convert.ToSingle(Weight.Text);
+
+                if(h > 10)
+                {
+                    h = h / 100;
+                }
+
+                float bmi = w / (h * h);
+
+                WelcomeText.Text = bmi.ToString();
+                if(bmi < 18.5)
+                {
+                    FatOrThin.Text = "You are too thin!";
+                    GainOrLose.Text = "You have to gain some kilos!";
+                }
+                else if (bmi > 18.5 && bmi < 24.9)
+                {
+                    FatOrThin.Text = "You are normal!";
+                    GainOrLose.Text = "Keep staying like that!";
+                }
+                else if (bmi > 25 && bmi < 29.9)
+                {
+                    FatOrThin.Text = "You are overweight!";
+                    GainOrLose.Text = "You have to do exercise!";
+                }
+                else
+                {
+                    FatOrThin.Text = "You are obese!";
+                    GainOrLose.Text = "You have to lose some kilos!";
+                }
+            }
+            catch (FormatException)
+            {
+                WelcomeText.Text = "Wrong Number Provided!";
+            }
+            }
+
+        private void HeWe_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(Height.Text) && !string.IsNullOrEmpty(Weight.Text) 
+                && float.TryParse(Height.Text, out float _) 
+                && float.TryParse(Weight.Text, out float _))
+            {
+                CalculateBMI.IsEnabled = true;
+            }
+            else
+            {
+                CalculateBMI.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ Xamarin/AppDeneme/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppDeneme
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        public void Button_Clicked(object sender, EventArgs e)
+        {
+            Random rast = new Random();
+            int rand = rast.Next(3, 11);
+            Com_Guess.Text = (rand*3 + 1).ToString();
+            Clicked.IsVisible = false;
+            Clicked2.IsVisible = true;
+            Guess.IsVisible = true;
+        }
+        public void Button_Clicked2(object sender, EventArgs e)
+        { 
+            int rand;
+            int gu = Convert.ToInt32(Guess.Text);
+            if ((Convert.ToInt32(Com_Guess.Text) - Convert.ToInt32(Guess.Text)) < 2 |
+                (Convert.ToInt32(Com_Guess.Text) - Convert.ToInt32(Guess.Text)) > 0 |
+                (Convert.ToInt32(Com_Guess.Text) != Convert.ToInt32(Guess.Text)))
+            {
+                YourGuess.Text = "Tahmininiz = " + gu;
+                Error_Guess.IsVisible = true;
+                Error_Guess.Text = "Lütfen Ekrandaki Sayının 1 ya da 2 Eksiğini Girin!";
+            }
+            if (Convert.ToInt32(Guess.Text) <= 0)
+            {
+                Sonuc.Text = "Kaybettiniz. Ancak Kazanamazdınız :)";
+                YourGuess.Text = "Tahmininiz = " + gu;
+                Clicked2.IsVisible = false;
+                Guess.IsVisible = false;
+                Error_Guess.IsVisible = false;
+                Basla.IsVisible = false;
+                Com_Guess.IsVisible = false;
+                Label2.IsVisible = false;
+            }
+            else
+            {
+                if ((Convert.ToInt32(Com_Guess.Text) - Convert.ToInt32(Guess.Text)) == 1)
+                {
+                    Com_Guess.Text = Guess.Text;
+                    rand = Convert.ToInt32(Com_Guess.Text) - 2;
+                    Com_Guess.Text = rand.ToString();
+                    Error_Guess.IsVisible = false;
+                    Guess.Text = string.Empty;
+                    YourGuess.Text = "Tahmininiz = " + gu;
+                }
+                else if ((Convert.ToInt32(Com_Guess.Text) - Convert.ToInt32(Guess.Text)) == 2)
+                {
+                    Com_Guess.Text = Guess.Text;
+                    rand = Convert.ToInt32(Com_Guess.Text) - 1;
+                    Com_Guess.Text = rand.ToString();
+                    Error_Guess.IsVisible = false;
+                    Guess.Text = string.Empty;
+                    YourGuess.Text = "Tahmininiz = " + gu;
+                }
+                if ((Convert.ToInt32(Com_Guess.Text) <= 0))
+                {
+                    Sonuc.Text = "Tebrikler Kazandınız. Nasıl Kazandın LAN! :)";
+                    Clicked2.IsVisible = false;
+                    Guess.IsVisible = false;
+                    Error_Guess.IsVisible = false;
+                    Basla.IsVisible = false;
+                    Com_Guess.IsVisible = false;
+                    Label2.IsVisible = false;
+                }
+            }
+        }
+
+        private void Guess_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Guess.Text) && float.TryParse(Guess.Text, out float _))
+            {
+                Clicked2.IsEnabled = true;
+            }
+        }
+    }
+    }
+
